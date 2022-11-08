f1d7738f7c98f1844256e52023bbaf963f323224
Amida Fombutu <amida.fombutu@gmail.com>
2022-11-08 10:03:26 -0600

    transfer commit

---
+++ keys.js
@@ -0,0 +1,4 @@
+  const MAPBOX_TOKEN = "pk.eyJ1IjoibWlkb3N0YXIxIiwiYSI6ImNsN3l5MHdnNzAwdmIzcGtlY2tkdmpjdjUifQ.J3-IBDspbluk4xWHVU6RxA"
+
+const OPEN_WEATHER_APPID = "458cb533d7e56f715f01f6725d45bdda"
+  const GIT_TOKEN = "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"
